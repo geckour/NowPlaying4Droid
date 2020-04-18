52626a976f681dfcb9986d031fbb5325e91329fc
geckour <geckour@gmail.com>
2020-04-19 00:55:47 +0900

    Add @JsonClass for SpotifyUserInfo

--- app/src/main/java/com/geckour/nowplaying4gpm/domain/model/SpotifyUserInfo.kt
+++ app/src/main/java/com/geckour/nowplaying4gpm/domain/model/SpotifyUserInfo.kt
@@ -1,8 +1,14 @@
 package com.geckour.nowplaying4gpm.domain.model
 
 import com.geckour.nowplaying4gpm.api.model.SpotifyToken
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class SpotifyUserInfo(
+    @Json(name = "token")
     val token: SpotifyToken,
+
+    @Json(name = "user_name")
     val userName: String
 )
