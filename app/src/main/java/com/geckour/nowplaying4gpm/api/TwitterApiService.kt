e7e76e4459398dbbf80318bcc22052124cfe9603
geckour <geckour@gmail.com>
2022-05-29 20:39:59 +0900

    Use v2 Twitter API

--- app/src/main/java/com/geckour/nowplaying4gpm/api/TwitterApiService.kt
+++ app/src/main/java/com/geckour/nowplaying4gpm/api/TwitterApiService.kt
@@ -2,11 +2,19 @@ package com.geckour.nowplaying4gpm.api
 
 import android.graphics.Bitmap
 import android.net.Uri
-import twitter4j.Status
+import twitter4j.CreateTweetResponse
 import twitter4j.auth.AccessToken
 
 interface TwitterApiService {
+
     suspend fun getRequestOAuthUri(): Uri?
+
     suspend fun getAccessToken(verifier: String): AccessToken?
-    suspend fun post(accessToken: AccessToken, subject: String, artwork: Bitmap?, artworkTitle: String?): Status?
+
+    suspend fun post(
+        accessToken: AccessToken,
+        subject: String,
+        artwork: Bitmap?,
+        artworkTitle: String?
+    ): CreateTweetResponse?
 }
