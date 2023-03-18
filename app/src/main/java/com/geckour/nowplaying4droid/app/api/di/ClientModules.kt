f4787e4eb7362c681cb49fb0413747c39f30e19b
geckour <geckour@gmail.com>
2023-03-18 12:21:57 +0900

    Bump up app version and dependencies

--- app/src/main/java/com/geckour/nowplaying4droid/app/api/di/ClientModules.kt
+++ app/src/main/java/com/geckour/nowplaying4droid/app/api/di/ClientModules.kt
@@ -1,13 +1,9 @@
 package com.geckour.nowplaying4droid.app.api.di
 
-import com.geckour.nowplaying4droid.BuildConfig
 import com.geckour.nowplaying4droid.app.api.LastFmApiClient
 import com.geckour.nowplaying4droid.app.api.MastodonInstancesApiClient
 import com.geckour.nowplaying4droid.app.api.SpotifyApiClient
 import com.geckour.nowplaying4droid.app.api.YouTubeDataClient
-import com.google.api.client.http.javanet.NetHttpTransport
-import com.google.api.client.json.jackson2.JacksonFactory
-import com.google.api.services.youtube.YouTube
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
